06af24797c62bf35a0ebe18dc0b9634f5ec435f3
letovesnoi <lenk.ff@gmail.com>
2018-10-01 20:40:53 +0300

    glue FP plots

--- paper_sim/extract-with-pandas.py
+++ paper_sim/extract-with-pandas.py
@@ -6,13 +6,13 @@ import pandas
 import matplotlib.pyplot as plt
 
 max_degree = 2
-postfix_i = "_transcript"
 postfix_g = "_gene"
+postfix_i = "_transcript"
 list_colors = ['blue', 'red', 'green', 'yellow', 'magenta', 'orange', 'cyan', 'black']
 
 # read simulated data file
 df_g = pandas.read_csv(sys.argv[1], delim_whitespace=True)
-df_t = pandas.read_csv(sys.argv[2], delim_whitespace=True)
+df_i = pandas.read_csv(sys.argv[2], delim_whitespace=True)
 
 def read_paths(txt_path):
     with open(txt_path, 'r') as fin:
@@ -65,23 +65,33 @@ def get_label(path):
     label = os.path.basename(path)[:i_ext]
     return label
 
-def plot_FP(paths_50, paths_95, df, postfix, name):
-    zero_cov_num = df[df.TPM == 0].shape[0]
-    plt.title('FP histogram\n (zero coverage genes {})'.format(zero_cov_num))
+def plot_FP(paths_g50, paths_g95, paths_i50, paths_i95,
+            df_g, df_i, postfix_g, postfix_i, name):
+    zero_cov_num_g = df_g[df_g.TPM == 0].shape[0]
+    zero_cov_num_i = df_i[df_i.TPM == 0].shape[0]
+    plt.title('FP histogram\n (zero coverage genes {}, isoforms {})'.
+              format(zero_cov_num_g, zero_cov_num_i))
     legend_text = []
-    x = np.array([0, 1])
-    for i_path in range(len(paths_50)):
-        path_50 = paths_50[i_path]
-        path_95 = paths_95[i_path]
+    x = np.array(range(4))
+    for i_path in range(len(paths_g50)):
+        legend_text.append(get_label(paths_g50[i_path]))
 
-        legend_text.append(get_label(path_50))
+        df_filtered_g50 = filter_by_assembled(paths_g50[i_path], df_g, postfix_g)
+        FP_g50 = df_filtered_g50[df_filtered_g50.TPM == 0].shape[0]
 
-        df_filtered_50 = filter_by_assembled(path_50, df, postfix)
-        df_filtered_95 = filter_by_assembled(path_95, df, postfix)
-        FP_50 = df_filtered_50[df_filtered_50.TPM == 0].shape[0]
-        FP_95 = df_filtered_95[df_filtered_95.TPM == 0].shape[0]
-        plt.plot(x, [FP_50, FP_95], '.', color=list_colors[i_path % len(list_colors)])
-    plt.xticks(x, ['50%-assembled', '95%-assembled'])
+        df_filtered_g95 = filter_by_assembled(paths_g95[i_path], df_g, postfix_g)
+        FP_g95 = df_filtered_g95[df_filtered_g95.TPM == 0].shape[0]
+
+        df_filtered_i50 = filter_by_assembled(paths_i50[i_path], df_i, postfix_i)
+        FP_i50 = df_filtered_i50[df_filtered_i50.TPM == 0].shape[0]
+
+        df_filtered_i95 = filter_by_assembled(paths_i95[i_path], df_i, postfix_i)
+        FP_i95 = df_filtered_i95[df_filtered_i95.TPM == 0].shape[0]
+
+        plt.plot(x, [FP_g50, FP_g95, FP_i50, FP_i95], '.', color=list_colors[i_path % len(list_colors)])
+
+    plt.xticks(x, ['50%-assembled\ngenes', '95%-assembled\ngenes',
+                   '50%-assembled\nisoforms', '95%-assembled\nisoforms'])
     plt.ylabel('FP')
     plt.legend(legend_text, fontsize='x-small', loc='center left', bbox_to_anchor=(1.01, 0.5))
     plt.savefig(name, additional_artists='art', bbox_inches='tight')
@@ -89,8 +99,7 @@ def plot_FP(paths_50, paths_95, df, postfix, name):
 
 plot_coverage_plot(paths_g50, df_g, postfix_g, '50%-behavior.genes.png')
 plot_coverage_plot(paths_g95, df_g, postfix_g, '95%-behavior.genes.png')
-plot_coverage_plot(paths_i50, df_t, postfix_i, '50%-behavior.isoforms.png')
-plot_coverage_plot(paths_i95, df_t, postfix_i, '95%-behavior.isoforms.png')
+plot_coverage_plot(paths_i50, df_i, postfix_i, '50%-behavior.isoforms.png')
+plot_coverage_plot(paths_i95, df_i, postfix_i, '95%-behavior.isoforms.png')
 
-plot_FP(paths_g50, paths_g95, df_g, postfix_g, 'FP.genes.png')
-plot_FP(paths_i50, paths_g95, df_t, postfix_i, 'FP.isoforms.png')
+plot_FP(paths_g50, paths_g95, paths_i50, paths_g95, df_g, df_i, postfix_g, postfix_i, 'FP.png')
