85fccc72bf006f05b68f97ec7fd99764e2126d26
letovesnoi <lenk.ff@gmail.com>
2018-10-01 20:24:48 +0300

    not for isoforms only

--- paper_sim/extract-with-pandas.py
+++ paper_sim/extract-with-pandas.py
@@ -6,11 +6,13 @@ import pandas
 import matplotlib.pyplot as plt
 
 max_degree = 2
-postfix = "_transcript"
+postfix_i = "_transcript"
+postfix_g = "_gene"
 list_colors = ['blue', 'red', 'green', 'yellow', 'magenta', 'orange', 'cyan', 'black']
 
 # read simulated data file
-df = pandas.read_csv(sys.argv[1], delim_whitespace=True)
+df_g = pandas.read_csv(sys.argv[1], delim_whitespace=True)
+df_t = pandas.read_csv(sys.argv[2], delim_whitespace=True)
 
 def read_paths(txt_path):
     with open(txt_path, 'r') as fin:
@@ -18,7 +20,12 @@ def read_paths(txt_path):
     paths = [path.strip() for path in paths]
     return paths
 
-def filter_by_assembled(path_assembled, df):
+paths_g50 = read_paths(sys.argv[3])
+paths_g95 = read_paths(sys.argv[4])
+paths_i50 = read_paths(sys.argv[5])
+paths_i95 = read_paths(sys.argv[6])
+
+def filter_by_assembled(path_assembled, df, postfix):
     # read 50 / 95%-assembled genes / isoforms
     good_transcripts_df = pandas.read_csv(path_assembled, header=None)
     good_transcripts = set(good_transcripts_df[0].str[0:-len(postfix)])
@@ -26,7 +33,7 @@ def filter_by_assembled(path_assembled, df):
     return df_filtered
 
 
-def plot_coverage_plot(paths, df):
+def plot_coverage_plot(paths, df, postfix, name):
     plt.title('Cumulative plot')
     legend_text = []
     for i_path in range(len(paths)):
@@ -37,7 +44,7 @@ def plot_coverage_plot(paths, df):
 
         bins = np.logspace(0, max_degree, max_degree + 1)
 
-        df_filtered = filter_by_assembled(path, df)
+        df_filtered = filter_by_assembled(path, df, postfix)
 
         groups_by_TPM = df_filtered.groupby(np.digitize(df_filtered.TPM, bins))
 
@@ -50,7 +57,7 @@ def plot_coverage_plot(paths, df):
     plt.xlabel('TPM')
     plt.ylabel('Number')
     plt.legend(legend_text, fontsize='x-small', loc='center left', bbox_to_anchor=(1.01, 0.5))
-    plt.savefig('cov_plot.png', additional_artists='art', bbox_inches='tight')
+    plt.savefig(name, additional_artists='art', bbox_inches='tight')
     plt.show()
 
 def get_label(path):
@@ -58,9 +65,9 @@ def get_label(path):
     label = os.path.basename(path)[:i_ext]
     return label
 
-def plot_FP(paths_50, paths_95, df):
+def plot_FP(paths_50, paths_95, df, postfix, name):
     zero_cov_num = df[df.TPM == 0].shape[0]
-    plt.title('FP histogram (zero coverage {})'.format(zero_cov_num))
+    plt.title('FP histogram\n (zero coverage genes {})'.format(zero_cov_num))
     legend_text = []
     x = np.array([0, 1])
     for i_path in range(len(paths_50)):
@@ -69,17 +76,21 @@ def plot_FP(paths_50, paths_95, df):
 
         legend_text.append(get_label(path_50))
 
-        df_filtered_50 = filter_by_assembled(path_50, df)
-        df_filtered_95 = filter_by_assembled(path_95, df)
+        df_filtered_50 = filter_by_assembled(path_50, df, postfix)
+        df_filtered_95 = filter_by_assembled(path_95, df, postfix)
         FP_50 = df_filtered_50[df_filtered_50.TPM == 0].shape[0]
         FP_95 = df_filtered_95[df_filtered_95.TPM == 0].shape[0]
         plt.plot(x, [FP_50, FP_95], '.', color=list_colors[i_path % len(list_colors)])
     plt.xticks(x, ['50%-assembled', '95%-assembled'])
     plt.ylabel('FP')
     plt.legend(legend_text, fontsize='x-small', loc='center left', bbox_to_anchor=(1.01, 0.5))
-    plt.savefig('FP.png', additional_artists='art', bbox_inches='tight')
+    plt.savefig(name, additional_artists='art', bbox_inches='tight')
     plt.show()
 
-plot_coverage_plot(read_paths(sys.argv[2]), df)
-plot_coverage_plot(read_paths(sys.argv[3]), df)
-plot_FP(read_paths(sys.argv[2]), read_paths(sys.argv[3]), df)
+plot_coverage_plot(paths_g50, df_g, postfix_g, '50%-behavior.genes.png')
+plot_coverage_plot(paths_g95, df_g, postfix_g, '95%-behavior.genes.png')
+plot_coverage_plot(paths_i50, df_t, postfix_i, '50%-behavior.isoforms.png')
+plot_coverage_plot(paths_i95, df_t, postfix_i, '95%-behavior.isoforms.png')
+
+plot_FP(paths_g50, paths_g95, df_g, postfix_g, 'FP.genes.png')
+plot_FP(paths_i50, paths_g95, df_t, postfix_i, 'FP.isoforms.png')
