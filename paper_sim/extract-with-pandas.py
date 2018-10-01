ada52a10442d2144f94bc88b010c54ed16b9e197
letovesnoi <lenk.ff@gmail.com>
2018-10-01 16:36:44 +0300

    different colors

--- paper_sim/extract-with-pandas.py
+++ paper_sim/extract-with-pandas.py
@@ -5,8 +5,9 @@ import numpy as np
 import pandas
 import matplotlib.pyplot as plt
 
-max_degree = 3
+max_degree = 2
 postfix = "_transcript"
+list_colors=['blue', 'red', 'green', 'yellow', 'magenta', 'orange', 'cyan', 'black']
 
 df = pandas.read_csv(sys.argv[1], delim_whitespace=True)
 
@@ -15,9 +16,9 @@ with open(sys.argv[2], 'r') as fin:
 paths = [path.strip() for path in paths]
 
 legend_text = []
-for path in paths:
-    ext = '.list'
-    i_ext = os.path.basename(path).find(ext)
+for i_path in range(len(paths)):
+    path = paths[i_path]
+    i_ext = os.path.basename(path).rfind('.')
     label = os.path.basename(path)[:i_ext]
     legend_text.append(label)
 
@@ -28,9 +29,11 @@ for path in paths:
     groups = df_filtered.groupby(np.digitize(df_filtered.TPM, bins))
 
     x = np.insert(bins, len(bins), 10 ** (max_degree + 1))
-    plt.plot(x, np.cumsum(groups.size()), 'ro')
+    plt.plot(x, np.cumsum(groups.size()), 'ro', color=list_colors[i_path % len(list_colors)])
     # plt.ylim(0, len(df_filtered) + 100)
     plt.xscale('symlog')
+    plt.yscale('log')
 
 plt.legend(legend_text, fontsize='x-small', loc='center left', bbox_to_anchor=(1.01, 0.5))
+plt.savefig('plot.png')
 plt.show()
